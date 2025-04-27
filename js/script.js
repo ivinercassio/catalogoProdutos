de29ac7dee984b30233bc11db29bd1dcaf11b3ec
ivinercassio <ivinercassio@gmail.com>
2025-04-27 14:55:44 -0300

    Products Page

--- js/script.js
+++ js/script.js
@@ -13,13 +13,11 @@ async function logar() {
     const response = await fetch(link)
     .then( async data => {
         let result = await data.json();
-        // save user on LocalStorage
-        console.log(result);
         if (result != null && result != "{}"){
             localStorage.setItem("userLogado", JSON.stringify(result));
             window.location.href = "home.html";
         }else
-            alert("VALIDAR: Usuário NÃO existe")
+            alert("VALIDAR: Usuário NÃO existe");
 
     }).catch( error => {
         console.error("API error: " + error);
@@ -35,4 +33,37 @@ function setUser() {
     let user = JSON.parse(localStorage.getItem("userLogado"));
     document.getElementById("userName").innerText = user.nome;
     document.getElementsByTagName("h2")[0].innerText = "Olá, " + user.nome;
+}
+
+async function getMyProducts() {
+    setUser();
+    document.getElementsByTagName("h2")[0].innerText = "Meus Produtos";
+
+    let user = JSON.parse(localStorage.getItem("userLogado"));
+    let link = api + "produto/" + user.chave + "/usuario";
+    
+    const response = await fetch(link)
+    .then( async data => {
+        let list = await data.json();
+        if (list != null && list != "{}"){
+            localStorage.setItem("products", JSON.stringify(list));
+            setProductsTable();
+        }
+        else
+            alert("VALIDAR: sem produtos cadastrados");
+
+    }).catch( error => {
+        console.error("API error: " + error);
+    });
+}
+
+function setProductsTable() {
+    let list = JSON.parse(localStorage.getItem("products"));
+    
+    list.forEach(item => {
+        let newRow = document.getElementsByTagName("tbody")[0].insertRow();
+        newRow.insertCell().textContent = item.descricao;
+        newRow.insertCell().textContent = item.valor;
+        newRow.insertCell().innerHTML = "<img src=" + item.urlImagem + "/>";
+    });
 }
