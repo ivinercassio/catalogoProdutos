756844617c4681f742472da1e4e5205207b713e2
ivinercassio <ivinercassio@gmail.com>
2025-05-06 16:27:50 -0300

    Alert Page Table

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 var api = "https://api-odinline.odiloncorrea.com/";
-
 // object user: 
 // {id: 6, nome: 'Usuario Teste', login: 'teste', email: 'usuarioTeste@gmail.com', chave: '61589-6'}
 
+// index page
 async function logar() {
     let login = $("#login").val();
     let password = $("#password").val();
@@ -24,17 +24,20 @@ async function logar() {
     });
 }
 
+// all pages
 function logout() {
-    if(localStorage.getItem("userLogado"))
+    if(localStorage.getItem("userLogado")) 
         localStorage.removeItem("userLogado");
 }
 
+// home page
 function setHome() {
     let user = JSON.parse(localStorage.getItem("userLogado"));
     document.getElementById("userName").innerText = user.nome;
     $("h2")[0].innerText = "Olá, " + user.nome;
 }
 
+// my products page
 async function setMyProducts() {
     let user = JSON.parse(localStorage.getItem("userLogado"));
     document.getElementById("userName").innerText = user.nome;
@@ -62,48 +65,65 @@ function setProductsTable() {
     
     list.forEach(item => {
         let newRow = $("tbody")[0].insertRow();
+        newRow.insertCell().textContent = item.id;  
         newRow.insertCell().textContent = item.descricao;
         newRow.insertCell().textContent = item.valor;
         newRow.insertCell().innerHTML = "<img src=" + item.urlImagem + "/>";
     });
 }
 
-function setPriceAlert() {
+// price alert page
+async function setPriceAlert() {
     let user = JSON.parse(localStorage.getItem("userLogado"));
     document.getElementById("userName").innerText = user.nome;
 
     $("h2")[0].innerText = "Meus Alertas";
 
+    if (!localStorage.getItem("products")) {
+        let link = api + "produto/" + user.chave + "/usuario";
+
+        const response = await fetch(link)
+        .then( async data => {
+            let list = await data.json();
+            if (list != null && list != "{}")
+                localStorage.setItem("products", JSON.stringify(list));
+            else
+                alert("VALIDAR: sem produtos cadastrados");
+        }).catch( error => {
+            console.error("API error: " + error);
+        });
+    }
+
     let products = JSON.parse(localStorage.getItem("products"));
     let select = $("select")[0];
     products.forEach(item => {
-        select.add(new Option(item.descricao, item.id));
+        select.add(new Option(item.id + " - " + item.descricao, item.id));
     });
 
-    if(localStorage.getItem("alerts"))
+    if(localStorage.getItem("alerts") && localStorage.getItem("alerts") != "[]")
         setMyAlerts();
 }
 
 function setMyAlerts() {
     $("form")[0].insertAdjacentHTML("afterend", "<table class='table container mt-2'> </table>");
     $("table")[0].insertAdjacentHTML("afterbegin", "<thead> </thead> <tbody> </tbody>");
-    $("thead")[0].insertAdjacentHTML("afterbegin", "<tr> <th>Descrição</th> <th>Valor Desejado</th> <th>Ação</th> <th>Status</th> </tr>");
+    $("thead")[0].insertAdjacentHTML("afterbegin", "<tr> <th>Código</th> <th>Valor Desejado</th> <th>Ação</th> <th>Status</th> </tr>");
 
     let alerts = JSON.parse(localStorage.getItem("alerts"));
 
     alerts.forEach(item => {
         let newRow = $("tbody")[0].insertRow();
-        newRow.insertCell().textContent = item.descricao;
+        newRow.insertCell().textContent = item.idProduto;
         newRow.insertCell().textContent = item.valorDesejado;
         newRow.insertCell().textContent = item.acao;
-        newRow.insertCell().innerHTML = "<button class='btn btn-warning' type='button' onclick='deleteAlert('" + item.idProduto + "')'></button>";
+        newRow.insertCell().innerHTML = 
+        "<button class='btn btn-danger' type='button' id=" + item.idProduto + " onclick='deleteAlert(" + item.idProduto + ")'>Deletar</button>";
     });
 }
 
 function addAlert(){
     let idProduct = $("#product").val();
     let price = $("#price").val();
-    let descricao = JSON.parse(localStorage.getItem("products")).filter(p => p.id = idProduct)[0].descricao;
     document.getElementsByName("action").forEach( item => {
         if (item.checked) 
             action = item.value; 
@@ -111,22 +131,32 @@ function addAlert(){
 
     const newAlert = {
         idProduto: idProduct,
-        descricao: descricao,
         valorDesejado: price,
         acao: action
     };
-    
-    if(!localStorage.getItem("alerts")){
+    let alerts = [];
+
+    if(!localStorage.getItem("alerts") || localStorage.getItem("alerts") == "[]"){
         $("form")[0].insertAdjacentHTML("afterend", "<table class='table container mt-2'> </table>");
         $("table")[0].insertAdjacentHTML("afterbegin", "<thead> </thead> <tbody> </tbody>");
-        $("thead")[0].insertAdjacentHTML("afterbegin", "<tr> <th>Descrição</th> <th>Valor Desejado</th> <th>Ação</th> <th>Status</th> </tr>");
-    }
-
-    localStorage.setItem("alerts", JSON.stringify(newAlert));
+        $("thead")[0].insertAdjacentHTML("afterbegin", "<tr> <th>Código</th> <th>Valor Desejado</th> <th>Ação</th> <th>Status</th> </tr>");
+    }else 
+        alerts = JSON.parse(localStorage.getItem("alerts"));
+    
+    alerts.push(newAlert);
+    localStorage.setItem("alerts", JSON.stringify(alerts));
 
     let newRow = $("tbody")[0].insertRow();
-    newRow.insertCell().textContent = newAlert.descricao;
+    newRow.insertCell().textContent = newAlert.idProduto;
     newRow.insertCell().textContent = newAlert.valorDesejado;
     newRow.insertCell().textContent = newAlert.acao;
-    newRow.insertCell().innerHTML = "<button class='btn btn-warning' type='button' onclick='deleteAlert('" + newAlert.idProduto + "')'></button>";
+    newRow.insertCell().innerHTML = 
+    "<button class='btn btn-danger' type='button' id=" + newAlert.idProduto + " onclick='deleteAlert(" + newAlert.idProduto + ")'>Deletar</button>";
+}
+
+function deleteAlert(id) {
+    let alerts = JSON.parse(localStorage.getItem("alerts"));
+    alerts = alerts.filter(a => a.idProduto != id);
+    localStorage.setItem("alerts", JSON.stringify(alerts));
+    window.location.reload(true);
 }
